d0ee710c4c49a3e9b247fe8b044d897a5dcda241
Sébastien Gaudin <contact@ingredient-technique.com>
2020-12-13 18:36:41 +0100

    modification des templates, ajout d'un peu de style

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,4 +15,49 @@ class DefaultController extends AbstractController
     {
         return $this->render('default/index.html.twig');
     }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        return $this->render('default/about.html.twig');
+    }
+
+    /**
+     * @return Response
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('default/contact.html.twig');
+    }
+
+    /**
+     * @return Response
+     * @Route("/history", name="history")
+     */
+    public function history(): Response
+    {
+        return $this->render('default/historique.html.twig');
+    }
+
+
+    /**
+     * @return Response
+     * @Route("/tools", name="tools")
+     */
+    public function tools(): Response
+    {
+        return $this->render('default/outils.html.twig');
+    }
+
+    /**
+     * @return Response
+     * @Route("/knowledge", name="knowledge")
+     */
+    public function knowledge(): Response
+    {
+        return $this->render('default/connaissances.html.twig');
+    }
 }
